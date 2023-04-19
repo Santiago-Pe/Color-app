df8e7c18f2ad6de2e67fbf839760c14421f60b2f
Santiago-pe <penasantiago346@gmail.com>
2023-04-19 16:53:18 -0300

    Adding react color and form to creat new palette

--- src/Components/NewPaletteForm/NewPaletteForm.js
+++ src/Components/NewPaletteForm/NewPaletteForm.js
@@ -1,32 +1,26 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import { withStyles } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import React from "react";
+import classNames from "classnames";
+import { withStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import { ChromePicker } from "react-color";
+import { Button } from "@material-ui/core";
 
-const drawerWidth = 240;
+const drawerWidth = 400;
 
-const styles = theme => ({
+const styles = (theme) => ({
   root: {
-    display: 'flex',
+    display: "flex",
   },
   appBar: {
-    transition: theme.transitions.create(['margin', 'width'], {
+    transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
@@ -34,7 +28,7 @@ const styles = theme => ({
   appBarShift: {
     width: `calc(100% - ${drawerWidth}px)`,
     marginLeft: drawerWidth,
-    transition: theme.transitions.create(['margin', 'width'], {
+    transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
@@ -44,7 +38,7 @@ const styles = theme => ({
     marginRight: 20,
   },
   hide: {
-    display: 'none',
+    display: "none",
   },
   drawer: {
     width: drawerWidth,
@@ -54,23 +48,23 @@ const styles = theme => ({
     width: drawerWidth,
   },
   drawerHeader: {
-    display: 'flex',
-    alignItems: 'center',
-    padding: '0 8px',
+    display: "flex",
+    alignItems: "center",
+    padding: "0 8px",
     ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
+    justifyContent: "flex-end",
   },
   content: {
     flexGrow: 1,
     padding: theme.spacing.unit * 3,
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
     marginLeft: -drawerWidth,
   },
   contentShift: {
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
@@ -81,6 +75,7 @@ const styles = theme => ({
 class NewPaletteForm extends React.Component {
   state = {
     open: false,
+    background: "",
   };
 
   handleDrawerOpen = () => {
@@ -91,6 +86,14 @@ class NewPaletteForm extends React.Component {
     this.setState({ open: false });
   };
 
+  //   TEST
+  handleChange2 = (colors) => {
+    this.setState({ background: colors.rgb });
+  };
+
+  handleChangeComplete = (colors) => {
+    this.setState({ background: colors.rgb });
+  };
   render() {
     const { classes, theme } = this.props;
     const { open } = this.state;
@@ -129,10 +132,27 @@ class NewPaletteForm extends React.Component {
         >
           <div className={classes.drawerHeader}>
             <IconButton onClick={this.handleDrawerClose}>
-                <ChevronLeftIcon />
+              <ChevronLeftIcon />
             </IconButton>
           </div>
           <Divider />
+          <Typography variant="h4" >Desing your Palette</Typography>
+          <div>
+            <Button variant="contained" color="primary">
+              Clear palette
+            </Button>
+            <Button variant="contained" color="secondary">
+              Random color
+            </Button>
+          </div>
+          <ChromePicker
+            color={this.state.background}
+            onChange={this.handleChange2}
+            onChangeComplete={this.handleChangeComplete}
+          />
+          <Button variant="contained" color="secondary">
+            Add new color
+          </Button>
         </Drawer>
         <main
           className={classNames(classes.content, {
@@ -145,7 +165,5 @@ class NewPaletteForm extends React.Component {
     );
   }
 }
-
-
 
 export default withStyles(styles, { withTheme: true })(NewPaletteForm);
