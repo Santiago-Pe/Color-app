18d07b5669327c8c1a54fffa08596a629a82d6ef
Santiago-Pe <penasantiago364@gmail.com>
2023-04-26 13:06:04 -0300

    sacar stric mode

--- src/index.js
+++ src/index.js
@@ -2,17 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </BrowserRouter>
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
 
-reportWebVitals();
